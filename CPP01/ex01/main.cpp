17899d71026b218417cecd8ca9a38c4e7b53ce49
Jo-hsi TAN <jo-tan@paul-f4Ar5s1.clusters.42paris.fr>
2024-02-14 11:13:03 +0100

    fix norminette. Bug: NULL string passing

--- CPP01/ex01/main.cpp
+++ CPP01/ex01/main.cpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jo-tan <jo-tan@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/14 10:44:28 by jo-tan            #+#    #+#             */
+/*   Updated: 2024/02/14 11:12:16 by jo-tan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Zombie.hpp"
 
 int main(void)
@@ -20,6 +32,14 @@ int main(void)
         zombies[i].annouce();
     delete[] zombies;
     std::cout << std::endl;
+
+    // size = 1;
+    // std::cout << "=== summon horde of Zombies " << size << " times ===" << std::endl;
+    // zombies = zombieHorde(size, NULL);
+    // for(int i = 0; i < size; ++i)
+    //     zombies[i].annouce();
+    // delete[] zombies;
+    // std::cout << std::endl;
 
     return 0;
 }
