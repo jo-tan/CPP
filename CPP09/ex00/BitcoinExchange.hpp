fd7ba01c112887c6fea61e9cf25abcc0e87258b4
Jo-hsi TAN <jo-tan@made-f0Ar3s2.clusters.42paris.fr>
2024-09-10 10:31:29 +0200

    remove subject description

--- CPP09/ex00/BitcoinExchange.hpp
+++ CPP09/ex00/BitcoinExchange.hpp
@@ -6,26 +6,9 @@
 /*   By: jo-tan <jo-tan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/19 18:10:06 by jo-tan            #+#    #+#             */
-/*   Updated: 2024/08/19 18:10:19 by jo-tan           ###   ########.fr       */
+/*   Updated: 2024/09/10 10:30:25 by jo-tan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-/*You have to create a program which outputs the value of a certain amount of bitcoin
-on a certain date.
-This program must use a database in csv format which will represent bitcoin price
-over time. This database is provided with this subject.
-The program will take as input a second database, storing the different prices/dates
-to evaluate.
-Your program must respect these rules:
-• The program name is btc.
-• Your program must take a file as argument.
-• Each line in this file must use the following format: "date | value".
-• A valid date will always be in the following format: Year-Month-Day.
-• A valid value must be either a float or a positive integer, between 0 and 1000.
-
-You must use at least one container in your code to validate this
-exercise. You should handle possible errors with an appropriate
-error message.*/
 
 #pragma once
 
--- CPP09/ex01/RPN.hpp
+++ CPP09/ex01/RPN.hpp
@@ -6,41 +6,9 @@
 /*   By: jo-tan <jo-tan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/19 18:11:26 by jo-tan            #+#    #+#             */
-/*   Updated: 2024/08/19 18:11:28 by jo-tan           ###   ########.fr       */
+/*   Updated: 2024/09/10 10:29:21 by jo-tan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-/*You must create a program with these constraints:
-• The program name is RPN.
-• Your program must take an inverted Polish mathematical expression as an argu-
-ment.
-• The numbers used in this operation and passed as arguments will always be less
-than 10. The calculation itself but also the result do not take into account this rule.
-• Your program must process this expression and output the correct result on the
-standard output.
-• If an error occurs during the execution of the program an error message should be
-displayed on the standard output.
-• Your program must be able to handle operations with these tokens: "+ - / *".
-
-You must use at least one container in your code to validate this
-exercise.
-
-You don’t need to manage the brackets or decimal numbers.
-
-Here is an example of a standard use:
-$> ./RPN "8 9 * 9 - 9 - 9 - 4 - 1 +"
-42
-$> ./RPN "7 7 * 7 -"
-42
-$> ./RPN "1 2 * 2 / 2 * 2 4 - +"
-0
-$> ./RPN "(1 + 1)"
-Error
-$>
-
-Warning: The container(s) you used in the previous exercise are
-forbidden here. The container(s) you used to validate this exercise
-will not be usable for the rest of this module.*/
 
 #ifndef RPN_HPP
 # define RPN_HPP
